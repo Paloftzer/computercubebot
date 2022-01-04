16f44bafdb68275733662ce7d9655d5f6db1f1fa
Phillip Hansen <phille04@outlook.com>
2022-01-04 20:57:15 +0100

    Added welcome message with image on member join

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Takes info from discord.js package and puts it in the Discord variable
-const Discord = require('discord.js')
+const Discord = require("discord.js")
 
 // Load .env variables into a global variable which can be used anywhere with process.env
-require("dotenv").config
+require("dotenv").config()
+
+const generateImage = require("./generateImage")
 
 // Which events the bot is listening to
 const client = new Discord.Client({
   intents: [
     "GUILDS",
-    "GUILD_MESSAGES"
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS"
   ]
 })
 
@@ -22,6 +25,18 @@ client.on("messageCreate", (message) => {
   if (message.content == "hi"){
     message.reply("Hello World!")
   }
+})
+
+// Sets the welcome channel to the channel with the given id
+const welcomeChannelId = "928007012116226148"
+
+// Sends a welcome message with an image to newly joined members
+client.on("guildMemberAdd", async (member) => {
+  const img = await generateImage(member)
+  member.guild.channels.cache.get(welcomeChannelId).send({
+    content: `<@${member.id}> Welcome to the server`,
+    files: [img]
+  })
 })
 
 // Log in as bot with the token
